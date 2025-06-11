e35c6d56d788b7e4e1b4a024c47c3c981f5ed3f5
JP <developerjp@pm.me>
2025-06-11 01:50:40 -0400

    Add project files.

---
+++ MinIO-UltraClient/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MinIO_UltraClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
